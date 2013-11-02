bf3013718c053dc3c1cb6435a6d440eb6c6bc077
Apprentice <apprentice@dbc26.local>
2013-11-01 17:35:39 -0700

    made a bunch of random edits to better understand code, do not use this.

--- app.rb
+++ app.rb
@@ -16,12 +16,19 @@ helpers do
   alias_method :h, :escape_html
 end
 
+# INDEX - /posts/ - Show the collection of posts
 get '/' do
-  erb :index, :locals => { :session => session }
+  # @email = session[:email]
+  erb :index, :locals => { :email => session[:email] }
+  # erb :index, :locals => { :id => session[curr_user_id] }
 end
 
 post '/' do
-  curr_user = User.find_by email: params[:email]
+  curr_user = User.find_by_email params[:email]
+  # curr_user_id = User.find_by_email params[:email].id
+  p curr_user
+  # curr_user = User.find_by_email(params[:email])
+
   if !curr_user || curr_user.password != params[:password]
     erb :login_failed
   else
@@ -30,15 +37,23 @@ post '/' do
   end
 end
 
+# USER Sign up
 get '/sign_up' do
   erb :sign_up
 end
 
+# Session create
 post '/sign_up' do
-  User.create(:email => params[:email], :password => params[:password])
+  # User.create(:email => params[:email], :password => params[:password])
+  # p params
+  new_user = User.create(params)
+  session[:email] = params[:email]
+  # redirect '/'
   erb :confirm_new_user
+
 end
 
+# Session destroy
 get '/logout' do 
   session.clear
   redirect '/'
@@ -49,15 +64,22 @@ get '/search' do
 end
 
 post '/search' do
-  found_user = User.find_by email: params[:email]
+  found_user = User.find_by_email params[:email]
   if found_user
+    p found_user
     redirect '/' + found_user.id.to_s
   else
     flash[:notice] = "Sorry, we don't have any users with that email address."
+    p flash
+    p flash[:notice]
+    puts "now some sessions!"
+    p session
+    p session[:flash]
     redirect '/search'
   end
 end
 
+# SHOW - /posts/:id/ - Shows an instance of a post
 get '/:page_id' do
   @feed_owner = User.find_by id: params[:page_id]
   if !session[:email]
@@ -71,6 +93,7 @@ get '/:page_id' do
   end
 end
 
+# CREATE - Create a new instance and pass the user along
 post '/:page_id' do
   @feed_owner = User.find_by id: params[:page_id]
   Post.create(:title => params[:title], :body => params[:body],
@@ -78,6 +101,7 @@ post '/:page_id' do
   erb :user_feed
 end
 
+# NEW - Display a form which can be used for creation
 get '/:page_id/new' do
   @feed_owner = User.find_by id: params[:page_id]
   if @feed_owner.email == session[:email]
